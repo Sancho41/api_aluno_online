a97a661a5c01ef51b96d2cbb167f429f8cf08ef6
Gabriel Sancho <gabriel.sancho13@gmail.com>
2020-09-09 19:54:48 -0300

    Criando projeto com Gradle

---
+++ src/Application.kt
@@ -0,0 +1,51 @@
+package dev.panelinha
+
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.request.*
+import io.ktor.features.*
+import io.ktor.routing.*
+import io.ktor.http.*
+import io.ktor.auth.*
+import io.ktor.gson.*
+import io.ktor.client.*
+import io.ktor.client.engine.apache.*
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // Referenced in application.conf
+@kotlin.jvm.JvmOverloads
+fun Application.module(testing: Boolean = false) {
+    install(CORS) {
+        method(HttpMethod.Options)
+        method(HttpMethod.Put)
+        method(HttpMethod.Delete)
+        method(HttpMethod.Patch)
+        header(HttpHeaders.Authorization)
+        header("MyCustomHeader")
+        allowCredentials = true
+        anyHost() // @TODO: Don't do this in production if possible. Try to limit it.
+    }
+
+    install(Authentication) {
+    }
+
+    install(ContentNegotiation) {
+        gson {
+        }
+    }
+
+    val client = HttpClient(Apache) {
+    }
+
+    routing {
+        get("/") {
+            call.respondText("HELLO WORLD!", contentType = ContentType.Text.Plain)
+        }
+
+        get("/json/gson") {
+            call.respond(mapOf("hello" to "world"))
+        }
+    }
+}
+
---
+++ src/dev/panelinha/models/User.kt
@@ -0,0 +1,9 @@
+package dev.panelinha.dev.panelinha.models
+
+class User {
+    var nome: String? = null
+    var semestre: Int? = null
+    var matricula: String? = null
+    var senha: String? = null
+    var dataNascimento: String? = null
+}
