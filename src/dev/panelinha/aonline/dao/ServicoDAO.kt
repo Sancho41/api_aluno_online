a1d91a1bd7c393eec4e8d39d1a0022ffcd17856d
raulc00 <jperon.gaming@gmail.com>
2020-09-17 18:52:54 -0300

    Serviços/Agendamento arrumado

--- src/dev/panelinha/aonline/dao/ServicoDAO.kt
+++ src/dev/panelinha/aonline/dao/ServicoDAO.kt
@@ -1,11 +1,33 @@
 package dev.panelinha.dev.panelinha.aonline.dao
 
+import dev.panelinha.aonline.dao.AuthDAO
+import org.jsoup.Jsoup
+
 class ServicoDAO {
 
     fun agendamentos(): List<Map<String, String>>{
 
-        return listOf(mapOf("qualquer coisa" to "outra coisa"))
+        val auth = AuthDAO()
+        val loginCookies = auth.login(" ", " ")
 
+        val atendimento = Jsoup.connect("http://apps.iesb.br/atendimentoagendado/atendimento/newatendimento")
+                .data("tipo", "aluno")
+                .data("categoria", "")
+                .data("codigo", "UU8wQURNekVqTXhjVE0")
+                .post()
 
+        val headers = atendimento.select("#home .classdatatable > thead > tr > th").map{ if (it.text() != "") it.text() else "actions"  }
+
+        val rows = atendimento.select("#home .classdatatable > tbody > tr");
+
+        return rows.map {
+
+            val requerimentos: MutableMap<String, String> = mutableMapOf()
+            val row = it.select("td")
+            for((index, head) in headers.withIndex())
+                requerimentos[head] = row[index].text()
+            requerimentos
+
+        }
     }
 }
