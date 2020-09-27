13ad8fc7b8023e2a41f4217c4b5a3597027125e0
Gabriel Sancho <gabriel.sancho13@gmail.com>
2020-09-26 22:23:52 -0300

    Ajuste apenas para compilação

--- src/dev/panelinha/aonline/dao/FinanceiroDAO.kt
+++ src/dev/panelinha/aonline/dao/FinanceiroDAO.kt
@@ -5,17 +5,15 @@ import dev.panelinha.aonline.dao.AuthDAO
 import dev.panelinha.aonline.models.User
 import org.jsoup.Jsoup
 
-class FinanceiroDAO {
-    fun extratoFin(user: User): List<>{
-        val auth = AuthDAO()
-        val loginCookies = auth.getCookies(user.matricula, user.senha)
+class FinanceiroDAO: PageDAO() {
+    fun extratoFin(user: User): List<Any>{
 
-        val extrato = Jsoup.connect("http://online.iesb.br/aonline/extrato_financeiro.asp")
-                .cookies(loginCookies)
+        val extrato = getConnection("http://online.iesb.br/aonline/extrato_financeiro.asp", user)
                 .data("dataIni", "01/01/2019")
                 .data("dataFim", "26/01/2020")
                 .post()
 
+        return listOf()
         //val headers = adaptação para extrato
     }
 }
