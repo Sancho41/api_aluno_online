f4b79acf5308e338169be74bf303e6b3d75807f4
Gabriel Sancho <gabriel.sancho13@gmail.com>
2020-09-30 20:07:33 -0300

    Remoção de métodos não utilizados

--- src/dev/panelinha/aonline/dao/AcademicoDAO.kt
+++ src/dev/panelinha/aonline/dao/AcademicoDAO.kt
@@ -26,12 +26,12 @@ class AcademicoDAO: PageDAO() {
         }
     }
 
+    // TODO
     fun atvCompl(user: User) {
         val atividades = getConnection(
                 "https://apps.iesb.br/sistemasAcademicos/application/modules/aonline/views/atividadesComplementares/js/atividades-complementares.js",
                 user
         ).get()
-
         //fazer adaptação para pegar o data e trazer informações das atividades
         //val atvDisponiveis = atividades
         //        .select("#nav nav-tabs list-unstyled components > li > a") nao sei se é melhor pegar a DATA ou pegar direto as colunas
@@ -66,57 +66,4 @@ class AcademicoDAO: PageDAO() {
 
         return horarios
     }
-
-    fun historicoAcad(user: User): Map<String, String> {
-
-        val historicoAcad = getConnection("http://online.iesb.br/aonline/historico.asp", user).get()
-
-        val rows = historicoAcad.select("#ctnTabPagina2 > table > tbody > tr > td > table > tr")
-
-        val info = rows.filter { !it.hasAttr("colspan") }.map { it.text() }
-
-        val infoMap: MutableMap<String, String> = mutableMapOf<String, String>()
-
-        info.forEach {
-            val listChaveValor = it.split(":")
-            if (listChaveValor.size >= 2) {
-                val (chave, valor) = listChaveValor
-                infoMap[chave] = valor
-            }
-        }
-
-        return infoMap
-    }
-
-    fun historicoAcadDisciplinasPendentes(user: User): List<Map<String, String>>{
-
-        val historicoAcadDisciplinasPendentes = getConnection("http://online.iesb.br/aonline/historico.asp", user).get()
-
-        val rows = historicoAcadDisciplinasPendentes.select("#ctnTabPagina2 > table > tbody > tr > td > b > b > b > b > b > b > table > tbody > tr")
-
-        val headers = rows[0].select("td").map {it.text()}
-
-        val materias = mutableListOf<Map<String, String>>()
-
-        for(i in 1 until (rows.size - 1)){
-
-            val row = rows[i].select("td")
-
-            if(row.size == headers.size){
-
-                val materia = mutableMapOf<String, String>()
-
-                for((index, valor) in headers.withIndex()){
-
-                    materia[valor] = row[index].text()
-                }
-
-                materias.add(materia)
-            }
-        }
-
-        return materias
-    }
-
 }
-
