9b2e0e881304b17d625857dfb1fb1f610db5b39d
Gabriel Sancho <gabriel.sancho13@gmail.com>
2020-11-12 22:32:05 -0300

    Adicionado variavel de ambiente para SECRET

--- src/dev/panelinha/aonline/modules/JwtConfig.kt
+++ src/dev/panelinha/aonline/modules/JwtConfig.kt
@@ -6,7 +6,7 @@ import com.auth0.jwt.algorithms.Algorithm
 import dev.panelinha.aonline.models.User
 
 object JwtConfig {
-    private const val secret = "My-super-secrete-secrete"
+    private val secret: String = System.getenv("SECRET") ?: "My-super-secrete-secrete"
     private const val issuer = "dev.panelinha"
     private val algorithm = Algorithm.HMAC512(secret)
 
